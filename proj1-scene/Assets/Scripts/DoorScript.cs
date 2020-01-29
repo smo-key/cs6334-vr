edcd856c91e679856a89b9c8e5a901061987457a
ShurongTian <sxt151030@utdallas.edu>
2020-01-28 21:57:29 -0600

    door added

---
+++ proj1-scene/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            anim.SetBool("open", true);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
