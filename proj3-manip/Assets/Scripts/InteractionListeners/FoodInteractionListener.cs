d6ca12eca6887b401ab25cd004918ed57625ce23
Saran Sundararajan <saransszg@gmail.com>
2020-03-11 20:13:16 -0500

    OnEnter works

--- proj3-manip/Assets/Scripts/InteractionListeners/FoodInteractionListener.cs
+++ proj3-manip/Assets/Scripts/InteractionListeners/FoodInteractionListener.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class FoodInteractionListener : MonoBehaviour, IInteractionListener
 {
+
+    public Material[] materials;
+
     public void OnFrame(InteractionController controller)
     {
         //attach food item position to hand mesh's position and rotation every frame if grabbed
@@ -11,12 +14,21 @@ public class FoodInteractionListener : MonoBehaviour, IInteractionListener
     public void OnEnterClosest(InteractionController controller)
     {
         //highlight it
-        Debug.Log("Enter closest: " + controller.DistanceFromObject(this.gameObject));
+        //Debug.Log("Enter closest: " + controller.DistanceFromObject(this.gameObject));
+        var child = this.gameObject.transform.GetChild(0);
+        print("Closest object is " + child.name);
+        var renderer = child.GetComponent<MeshRenderer>();
+        renderer.material = materials[0];
+
     }
 
     public void OnLeaveClosest(InteractionController controller)
     {
         //unhighlight it
+        var child = this.gameObject.transform.GetChild(0);
+        print("Closest object is " + child.name);
+        var renderer = child.GetComponent<MeshRenderer>();
+        renderer.material = materials[1];
     }
 
     public void OnGrab(InteractionController controller)
