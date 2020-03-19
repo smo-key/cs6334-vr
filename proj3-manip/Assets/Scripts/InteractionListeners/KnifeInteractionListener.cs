af0a54091c42873c58034d55726047f58ab0906b
Arthur Pachachura <pachachura.arthur@gmail.com>
2020-03-19 13:37:54 -0500

    Add knife interaction listener

---
+++ proj3-manip/Assets/Scripts/InteractionListeners/KnifeInteractionListener.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class KnifeInteractionListener : InteractionListener
+{
+    public Material DefaultMaterial;
+    public Material SelectedMaterial;
+    
+    bool IsGrabbed = false;
+    MeshRenderer ObjectRenderer;
+    Rigidbody rb;
+
+    public void Start()
+    {
+        ObjectRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    public override void OnFrame(InteractionController controller)
+    {
+        //attach food item position to hand mesh's position and rotation every frame if grabbed
+        if (IsGrabbed)
+        {
+            var hand = controller.GetHand();
+            var handRenderer = hand.GetComponent<MeshRenderer>();
+            gameObject.transform.position = controller.Target.transform.position;
+            handRenderer.enabled = false;
+        }
+    }
+
+    public override void OnEnterClosest(InteractionController controller)
+    {
+        //highlight it
+        ObjectRenderer.material = SelectedMaterial;
+    }
+
+    public override void OnLeaveClosest(InteractionController controller)
+    {
+        //unhighlight it
+        ObjectRenderer.material = DefaultMaterial;
+    }
+
+    public override void OnGrab(InteractionController controller)
+    {
+        var hand = controller.GetHand();
+        var handRenderer = hand.GetComponent<MeshRenderer>();
+
+        //make the hand mesh invisible
+        IsGrabbed = true;
+        handRenderer.enabled = false;
+        rb.isKinematic = true;
+    }
+
+    public override void OnDrop(InteractionController controller)
+    {
+        var hand = controller.GetHand();
+        var handRenderer = hand.GetComponent<MeshRenderer>();
+
+        //remove food item
+        IsGrabbed = false;
+        handRenderer.enabled = true;
+        rb.isKinematic = false;
+    }
+}
